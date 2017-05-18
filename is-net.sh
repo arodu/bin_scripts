6413cc3a3b3205316d3114d473f37ba666c8bf36
Alberto Rodriguez <arodu.dev@gmail.com>
2017-05-18 13:54:41 -0400

    update is-net

--- is-net.sh
+++ is-net.sh
@@ -2,6 +2,8 @@
 
 if [[ "$1" == "--panel" ]]; then
 	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && echo "<txt><span foreground=\"#FFF\">Online</span></txt>" || echo "<txt><span foreground=\"#F00\">Offline</span></txt>"
+elif [[ "$1" == "--notify" ]]; then
+	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && notify-send "Net" "Online" -i /usr/share/pixmaps/pidgin/emblems/scalable/external.svg || notify-send "Net" "Offline" -i /usr/share/pixmaps/pidgin/emblems/scalable/not-authorized.svg
 else
 	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && echo "Online" || echo "Offline"
 fi
