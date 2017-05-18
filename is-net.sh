9eb4386f4ac58eee9be6bcbbee776d9ff344f28e
Alberto Rodriguez <arodu.dev@gmail.com>
2017-05-18 11:24:33 -0400

    update is-net

--- is-net.sh
+++ is-net.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-ping -q -w 1 -c 1 8.8.8.8 > /dev/null && echo "<txt><span foreground=\"#FFF\">Online</span></txt>" || echo "<txt><span foreground=\"#F00\">Offline</span></txt>"
+if [[ "$1" == "--panel" ]]; then
+	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && echo "<txt><span foreground=\"#FFF\">Online</span></txt>" || echo "<txt><span foreground=\"#F00\">Offline</span></txt>"
+else
+	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && echo "Online" || echo "Offline"
+fi
