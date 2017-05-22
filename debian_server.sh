a27c4ee00661960d969bf52cd9ba8096d74dc72e
Alberto Rodriguez <arodu.dev@gmail.com>
2017-05-22 09:25:19 -0400

    start debian virtual machine

---
+++ debian_server.sh
@@ -0,0 +1,3 @@
+#! /bin/env bash
+
+VBoxManage startvm "debian" --type headless
--- is-net.sh
+++ is-net.sh
@@ -3,8 +3,7 @@
 if [[ "$1" == "--panel" ]]; then
 	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && echo "<txt><span foreground=\"#FFF\">Online</span></txt>" || echo "<txt><span foreground=\"#F00\">Offline</span></txt>"
 elif [[ "$1" == "--notify" ]]; then
-	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && notify-send "Net online" -i /usr/share/pixmaps/pidgin/emblems/scalable/external.svg || notify-send "Net offline" -i 
-/usr/share/pixmaps/pidgin/emblems/scalable/not-authorized.svg
+	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && notify-send "Net online" -i /usr/share/pixmaps/pidgin/emblems/scalable/external.svg || notify-send "Net offline" -i /usr/share/pixmaps/pidgin/emblems/scalable/not-authorized.svg
 else
 	ping -q -w 1 -c 1 8.8.8.8 > /dev/null && echo "Online" || echo "Offline"
 fi
