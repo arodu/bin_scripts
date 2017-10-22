c40eba29513907a24fb7d0bfce1545a77cdda085
Alberto Rodriguez <arodu.dev@gmail.com>
2017-10-22 13:57:46 -0400

    add calculator script

--- checkbtc.php
+++ checkbtc.php
@@ -1,10 +1,18 @@
 #!/usr/bin/php
 <?php
   $result = file_get_contents("https://localbitcoins.com/bitcoinaverage/ticker-all-currencies/");
-  $data = json_decode($result, true);
-  $value = $argv[1]*$data["VEF"]["avg_1h"];
+  $localbitcoins = json_decode($result, true);
+	$result = file_get_contents("https://api.coinmarketcap.com/v1/ticker/bitcoin/");
+  $coinmarketcap = json_decode($result, true);
+	//$result = file_get_contents("https://s3.amazonaws.com/dolartoday/data.json");
+  //$dolartoday = json_decode($result, true);
 
-  //var_dump($data["VEF"]);
-  echo "Price last 1h: ".number_format( $data["VEF"]["avg_1h"], 2, ",", ".")."\n";
-  echo "VEF: ".number_format( $value, 2, ",", ".")."\n";
+  $value = $argv[1]*$localbitcoins["VEF"]["avg_1h"];
+
+  //var_dump($dolartoday);
+	echo "BTC = $ ".$coinmarketcap[0]["price_usd"]."\n";
+	//echo "USD = Bs. ".$dolartoday["USD"]["dolartoday"]."\n";
+  echo "BTC = Bs. ".number_format( $localbitcoins["VEF"]["avg_1h"], 2, ",", ".")."\n";
+	echo "-------------------------\n";
+  echo "Bs. ".number_format( $value, 2, ",", ".")."\n";
 ?>
