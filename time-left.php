6efbcfb9785d96af31c32095a132eb5d2a2d7951
Alberto Rodriguez <arodu.dev@gmail.com>
2018-03-10 10:13:55 -0400

    update time-left

--- time-left.php
+++ time-left.php
@@ -1,7 +1,10 @@
 #!/usr/bin/php
-
 <?php
   date_default_timezone_set('America/Caracas');
+
+  // argv[1]: tiempo guardado en minutos
+  // argv[2]: dias a calcular, o dias del mes, por defecto 1
+  // argv[3]: horas por dia, por defecto 8
 
   $time = $argv[1];
   $days = (isset($argv[2]) ? $argv[2] : 1);
@@ -11,10 +14,8 @@
   $h = (int)($time_left/60);
   $m = $time_left - ($h*60);
 
-  echo "Time Left: ".$h."h ".$m."m\n";
-   
+  echo "Time Left: ".$h."h ".$m."m\n";  
   // ---------------------
-
   $estimate = date("h:ia", time()+($time_left*60));
-  echo "Estimate: ".$estimate;
+  echo "Estimate: ".$estimate."\n";
 ?>
