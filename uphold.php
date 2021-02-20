20bc8ada23c18c5c5a6cc93e09649b15be18fdbd
Alberto Rodriguez <arodu.dev@gmail.com>
2021-02-19 20:19:21 -0400

    update

--- uphold.php
+++ uphold.php
@@ -7,11 +7,11 @@
   $value = isset($argv[2]) ? floatval($argv[2]) : 1;
   $currency_base = isset($argv[3]) ? mb_strtoupper($argv[3]) : "USD";
 
-  $uphold_result = json_decode(utf8_decode(file_get_contents("https://api.uphold.com/v0/ticker/" . $currency_value.$currency_base, true)), true);
+  $uphold_result = json_decode(utf8_decode(file_get_contents("https://api.uphold.com/v0/ticker/" . $currency_value.'-'.$currency_base, true)), true);
 
   $price = $uphold_result['bid'];
 
-  echo "= ".$currency_base." ".number_format($price*$value, 2,'.',' ')."\n";
+  echo "= ".$currency_base." ".number_format($price*$value, 2,',','.')."\t\t".$currency_base." ".number_format($price*$value, 2,'.','')."\n";
 
 
   /*
